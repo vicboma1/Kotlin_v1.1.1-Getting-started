600a1a5c2e58076bee88ecbc4782d5df4af85ff4
Victor Bolinches <vicboma1@users.noreply.github.com>
2017-05-09 22:56:04 +0200

    Adding Java project

---
+++ java/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.stream.IntStream;
+
+/**
+ * Created by vicboma on 09/05/17.
+ */
+public class Main {
+
+    public static void main(String args[]){
+        invoke();
+    }
+
+    public static void invoke() {
+        IntStream.rangeClosed(1,99)
+                  .forEach( it -> {
+                      final String Empty = "";
+                      String str = Empty;
+                      if(it % 3 == 0 ) str += "Fizz";
+                      if(it % 5 == 0 ) str += "Buzz";
+                      if(str.equals(Empty)) str+= it;
+                      System.out.print(str.concat(" "));
+        });
+
+    }
+
+}
